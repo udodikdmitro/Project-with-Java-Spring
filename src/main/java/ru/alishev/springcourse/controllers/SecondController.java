364040a8641f0ebc1de2d17a36cd1ca7da045f05
dima udodik <dima udodik>
2023-08-06 20:42:42 +0300

    GET request.20

--- src/main/java/ru/alishev/springcourse/controllers/SecondController.java
+++ src/main/java/ru/alishev/springcourse/controllers/SecondController.java
@@ -3,9 +3,6 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * @author Neil Alishev
- */
 @Controller
 public class SecondController {
 
